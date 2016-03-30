4abfa033cacf3771e2e568c5330312d9f9456d69
Jake Franklin <jfranklin@telesphere.com>
2016-03-30 11:17:54 -0700

    updated beer model

--- models/beer.js
+++ models/beer.js
@@ -3,8 +3,18 @@ var mongoose = require('mongoose');
 // Define our beer schema
 var BeerSchema = new mongoose.Schema({
   name: String,
+  type: String,
   allergic: Boolean,
   ingredients: Array
+});
+
+// Taken from http://stackoverflow.com/questions/7034848/mongodb-output-id-instead-of-id
+// in regards to how backbone really prefers 'id' over '_id'
+BeerSchema.virtual('id').get(function() {
+	return this._id;
+});
+BeerSchema.set('toJSON', {
+    virtuals: true
 });
 
 // Export the Mongoose model
